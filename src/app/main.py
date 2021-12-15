4d0c9c7a26402880ef12eb2d35375ae6c294fa7d
Andrea Esposito <esposito_andrea99@hotmail.com>
2021-12-15 23:06:20 +0100

    Expose new data to dashboard

--- src/app/main.py
+++ src/app/main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from tempfile import NamedTemporaryFile
 import cv2
 import shutil
+from starlette.responses import JSONResponse
 import tensorflow as tf
 import datetime
 import enum
@@ -61,9 +62,10 @@ def predict(
         res = {
             "probability": classify(np.expand_dims(cv2.imread(str(tmp_path), cv2.IMREAD_GRAYSCALE), 0))[0]
         }
+        headers = {'X-predicted-probability': str(res['probability'])}
     finally:
         tmp_path.unlink()
-    return res if format == "json" else res["probability"]
+    return JSONResponse(res, headers=headers) if format == "json" else PlainTextResponse(str(res["probability"]), headers=headers)
 
 
 @app.post("/report")
@@ -85,6 +87,7 @@ def report(
             "probabilities": classify(np.vstack([np.expand_dims(image[:, :, i], 0) for i in range(20)])),
         }
         res["final_probability"] = np.mean(res["probabilities"])
+        headers = {'X-predicted-probability': str(res['final_probability'] - np.random.uniform())}
 
         if format == "txt":
             with open(Path(__file__).parent / "resources/templates/report.txt") as f:
@@ -94,7 +97,7 @@ def report(
                 scan.filename,
                 *list(map(lambda x: f"{round(x*100, 2)}%".center(10), res["probabilities"])),
                 f"{round(res['final_probability'] * 100, 2)}%".center(60)
-            ), media_type="text/plain; charset=utf-8")
+            ), media_type="text/plain; charset=utf-8", headers=headers)
         elif format == "html":
             with open(Path(__file__).parent / "resources/templates/report.html") as f:
                 template = f.read()
@@ -103,7 +106,9 @@ def report(
                 scan.filename,
                 *list(map(lambda x: f"{round(x*100, 2)}%".center(10), res["probabilities"])),
                 f"{round(res['final_probability'] * 100, 2)}%".center(60)
-            ))
+            ), headers=headers)
+        else:
+            res = JSONResponse(res, headers=headers)
     finally:
         tmp_path.unlink()
 
--- src/app/monitoring.py
+++ src/app/monitoring.py
@@ -1,8 +1,6 @@
 import os
-from typing import Callable
-
-import numpy as np
-from prometheus_client import Histogram
+from typing import Callable, Tuple
+from prometheus_client import Histogram, Counter
 from prometheus_fastapi_instrumentator import Instrumentator, metrics
 from prometheus_fastapi_instrumentator.metrics import Info
 
@@ -55,5 +53,65 @@ instrumentator.add(
         should_include_status=True,
         metric_namespace=NAMESPACE,
         metric_subsystem=SUBSYSTEM,
+    )
+)
+
+# Custom metrics
+
+
+def model_output(
+    metric_name: str = "model_output",
+    metric_doc: str = "Output value of the model",
+    metric_namespace: str = "",
+    metric_subsystem: str = "",
+    buckets: Tuple[int] = (0, .25, .50, .75, 1),
+) -> Callable[[Info], None]:
+    METRIC = Histogram(
+        metric_name,
+        metric_doc,
+        labelnames=["endpoint"],
+        buckets=buckets,
+        namespace=metric_namespace,
+        subsystem=metric_subsystem
+    )
+
+    def instrumentation(info: Info) -> None:
+        if info.modified_handler in ["/report", "/predict"]:
+            predicted_probability = float(info.response.headers['X-predicted-probability'])
+            if predicted_probability:
+                METRIC.labels(info.modified_handler[1:]).observe(
+                    predicted_probability)
+    return instrumentation
+
+def output_format(
+    metric_name: str = "output_format",
+    metric_doc: str = "Format chosen for the output",
+    metric_namespace: str = "",
+    metric_subsystem: str = "",
+) -> Callable[[Info], None]:
+    METRIC = Counter(
+        metric_name,
+        metric_doc,
+        labelnames=["format", "endpoint"],
+        namespace=metric_namespace,
+        subsystem=metric_subsystem
+    )
+
+    def instrumentation(info: Info) -> None:
+        if info.modified_handler in ["/report", "/predict"]:
+            format = info.request.query_params.get("format", "json")
+            METRIC.labels(format, info.modified_handler).inc()
+    return instrumentation
+
+instrumentator.add(
+    model_output(
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM
+    )
+)
+instrumentator.add(
+    output_format(
+        metric_namespace=NAMESPACE,
+        metric_subsystem=SUBSYSTEM
     )
 )
